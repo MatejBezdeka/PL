fe89acd7f627c9626318c237c65531a76bcc2cfb
Matěj Bezděka <matabezdeka@gmail.com>
2023-03-13 23:16:49 +0100

    13.3.23 23:16
    imported demos + MM tweaks

--- Assets/RetroAesthetics/Scripts/UVScroller.cs
+++ Assets/RetroAesthetics/Scripts/UVScroller.cs
@@ -2,22 +2,22 @@
 
 namespace RetroAesthetics {
 	public class UVScroller : MonoBehaviour {
-		public Vector2 scrollSpeed = new Vector2(-1f, 0f);
-		public string textureName = "_GridTex";
-		private Material target;
-		private Vector2 offset = Vector2.zero;
+		[SerializeField]Vector2 scrollSpeed = new Vector2(-1f, 0f);
+		[SerializeField]string textureName = "_GridTex";
+		Material target;
+		Vector2 offset = Vector2.zero;
 
 		void Start () {
 			var renderer = GetComponent<Renderer>();
 			if (renderer == null || renderer.material == null) {
-				this.enabled = false;
+				enabled = false;
 				return;
 			}
 
 			target = renderer.material;
 			if (!target.HasProperty(textureName)) {
 				Debug.LogWarning("Texture name '" + textureName + "' not found in material.");
-				this.enabled = false;
+				enabled = false;
 				return;
 			}
 		}
