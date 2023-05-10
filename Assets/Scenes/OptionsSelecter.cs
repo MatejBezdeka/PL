f938a74d8e29599c72dbed4932d515da0e8751aa
Matěj Bezděka <79998445+MatejBezdeka@users.noreply.github.com>
2023-05-10 11:12:23 +0200

    10.5.2023

--- Assets/Scenes/OptionsSelecter.cs
+++ Assets/Scenes/OptionsSelecter.cs
@@ -1,23 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OptionsSelecter : MonoBehaviour {
+    public enum type {
+        resolution,
+        quality,
+    }
     List<string> choices;
     int currentIndex = 0;
+    private TextMeshProUGUI text;
     [SerializeField] Button buttonRight;
     [SerializeField] Button buttonLeft;
     [SerializeField] bool cycleable;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        buttonRight.onClick.AddListener(ClickNext);
+        buttonLeft.onClick.AddListener(ClickPrev);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void ClickPrev() {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = choices.Count;
+        }
+        text.text = choices[currentIndex];
+        if (!cycleable && currentIndex == 0) {
+            buttonLeft.enabled = false;
+        }
+        else {
+            buttonLeft.enabled = true;
+        }
+    }
+
+    void ClickNext() {
+        currentIndex++;
+        if (currentIndex > choices.Count) {
+            currentIndex = 0;
+        }
+        text.text = choices[currentIndex];
+        if (!cycleable && currentIndex == choices.Count) {
+            buttonLeft.enabled = false;
+        }
+        else {
+            buttonLeft.enabled = true;
+        }
     }
 }
